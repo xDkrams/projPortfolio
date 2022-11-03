1bc290ba10ae8e6c990fe1a5d2392269c4630c2e
xDkrams <104502791+xDkrams@users.noreply.github.com>
2022-11-03 16:33:46 +0800

    feat: update minimal issues

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,37 +2,37 @@ import React from "react";
 import minigame from "../images/projects/minigame.png";
 import docTracker from "../images/projects/docTracker.png";
 import restoApp from "../images/projects/restoApp.jpg";
-import todos from "../images/projects/todos.png";
-
+import todo from "../images/projects/todo.png";
+import { AiFillGitlab } from "react-icons/ai";
 const Projects = () => {
   const projectPics = [
     {
       id: 1,
-      src: restoApp,
-      appName: "Resto App",
-      href: "https://restoapplication.herokuapp.com/",
-      link: "https://github.com/xDkrams/resto",
-    },
-    {
-      id: 2,
-      src: docTracker,
-      appName: "Incoming - Outgoing Doc tracker",
-      href: "",
-      link: "",
-    },
-    {
-      id: 3,
       src: minigame,
       appName: "Perya Color Game",
       href: "https://minigameapp.herokuapp.com/",
       link: "https://github.com/xDkrams/MiniGame/tree/main/projects-002-mini-game",
     },
     {
-      id: 4,
-      src: todos,
+      id: 2,
+      src: todo,
       appName: "Todo App",
       href: "https://todoapp025.herokuapp.com/",
       link: "https://github.com/xDkrams/Todo",
+    },
+    {
+      id: 3,
+      src: restoApp,
+      appName: "Resto App",
+      href: "https://restoapplication.herokuapp.com/",
+      link: "https://github.com/xDkrams/resto",
+    },
+    {
+      id: 4,
+      src: docTracker,
+      appName: "Incoming-Outgoing Doc tracker",
+      href: "",
+      link: "",
     },
   ];
   return (
@@ -47,29 +47,31 @@ const Projects = () => {
 
         <div className="grid sm:grid-cols-2 md: grid-cols-3 gap-8 px-12 sm:px-0">
           {projectPics.map(({ id, src, appName, href, link }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg ">
               <img
-                className="rounded-md duration-150 hover:scale-105"
+                className="rounded-md duration-300 hover:scale-105 my-2 h-64 "
                 src={src}
                 alt="resto"
               />
 
-              <div>
+              <div className="grid grid-cols-3 gap-2 ">
+                <button className="text-justify"> {`${appName}`} </button>
+
                 <a
                   href={href}
                   rel="noopener"
                   target="_blank"
-                  className="my-2 px-2 text-lg"
+                  className="my-3 p-4 text-xl font-bold text-blue-600 hover:underline underline-offset-8 hover:text-2xl"
                 >
-                  {appName}
+                  DEMO
                 </a>
                 <a
                   href={link}
                   rel="noopener"
                   target="_blank"
-                  className="my-2 px-2 text-lg"
+                  className="my-3 p-4 text-xl font-bold "
                 >
-                  Github
+                  <AiFillGitlab size={40} className="hover:scale-150	" />
                 </a>
               </div>
             </div>
